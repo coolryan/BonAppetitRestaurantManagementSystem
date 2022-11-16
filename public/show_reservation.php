4f23a48d3abd0bba61884a809e614ccb214edaa5
Ryan Setaruddin <coolry1998@gmail.com>
2022-11-16 15:11:02 -0500

    fixing two php files

--- public/show_reservation.php
+++ public/show_reservation.php
@@ -2,24 +2,26 @@
 <a href="/reservation.php" class="button">Review Reservation</a>
 <table>
 	<tr>
-		<th>Menu Item</th>
-		<th>Description</th>
-		<th>Category</th>
-		<th>Price</th>
-		<th>Active</th>
+		<th>Name</th>
+		<th>Party size</th>
+		<th>Email</th>
+		<th>Phone</th>
+		<th>Date</th>
+		<th>Time</th>
 	</tr>
 	<?php  
-		$qry_result = mysqli_query($conn, "SELECT * FROM menu_item")->fetch_all(MYSQLI_ASSOC);
-		foreach($qry_result as $menu_item)	{
-			$edit_link = "/editrersvationItem.php/?item={$menu_item['id']}";
+		$qry_result = mysqli_query($conn, "SELECT * FROM reservation_table")->fetch_all(MYSQLI_ASSOC);
+		foreach($qry_result as $reservation_item)	{
+			$edit_link = "/editrersvationItem.php/?item={$reservation_item['reservation_id']}";
 	?>
 	<tr>
-		<td><?= $menu_item['name'] ?></td>
-		<td><?= $menu_item['description'] ?></td>
-		<td><?= $menu_item['category'] ?></td>
-		<td><?= $menu_item['price'] ?></td>
-		<td><?= $menu_item['active'] ?></td>
-	
+		<td><?= $reservation_item['name'] ?></td>
+		<td><?= $reservation_item['party_size'] ?></td>
+		<td><?= $reservation_item['email'] ?></td>
+		<td><?= $reservation_item['phone'] ?></td>
+		<td><?= $reservation_item['date'] ?></td>
+		<td><?= $reservation_item['time'] ?></td>
+
 		<td><a href='<?= $edit_link ?>'>Edit</a></td>
 	</tr>
 	<?php  
