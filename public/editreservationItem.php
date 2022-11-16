feda97ac8f8375e565efb9e0fce76be9631b18e7
Ryan Setaruddin <coolry1998@gmail.com>
2022-11-15 20:05:42 -0500

    Fix the code

--- public/editreservationItem.php
+++ public/editreservationItem.php
@@ -8,80 +8,91 @@
 </head>
 <body>
 	<?php  
+		// Header
 		require_once("Header.php");
-		require_once("Connect.php");
 		require_once("utils.php");
 		checkAndStartSession();
+		$logged_in = isLoggedIn();
 
-		if (isset($_POST['name'])) {
-			$item_id = (empty($_POST['id'])) ? null : $_POST['id'];
-			$name = $_POST['name'];
-			$category = $_POST["category"];
-			$price = $_POST["price"];
-			$active = (isset($_POST["active"])) ? 1 : 0;
-
-			if($name != "" && $category != "" && $price != "") {
-				if($item_id == null) {
-					$qry = "INSERT INTO menu_item (id, name, price, active, category) VALUES (NULL, '{$name}', {$price}, {$active}, '{$category}')";
-				}
-				else {
-					$qry = "UPDATE menu_item SET name='{$name}', price={$price}, active={$active}, category='{$category}' WHERE id={$item_id}";
-				}
-
-				$qry_result = mysqli_query($conn, $qry);
-				if($qry_result) {
-					$success = True;
-				}
-			} else {
-				$error_msg = "Must enter all values";
-			}
-			if(isset($error_msg)) {
-				echo "<p color='red'>".$error_msg."</p>";
-			} else if(isset($success) && $success) {
-				echo "<p color='green'>Menu item saved!</p>";
-			}
-		} else if(!empty($_GET['item'])) {
-			$item_id = $_GET['item'];
-			$qry_result = mysqli_query($conn, "SELECT * FROM menu_item where id= {$item_id}");
-			$menu_item = mysqli_fetch_assoc($qry_result);
-			$name = $menu_item["name"];
-			$description = $menu_item["description"];
-			$category = $menu_item["category"];
-			$price = $menu_item["price"];
-			$active = ($menu_item["active"] >0) ? True : False;
-		} else {
-			$item_id = "";
-			$name = "";
-			$description = "";
-			$category = "";
-			$price = "";
-			$active = True;
-		}
+		// Connect to MySQL
+		require_once("Connect.php");
 	?>
-	<form action="editreserverationItem.php" class="form" method="POST">
-		<input name="id" type="hidden" value="<?= $item_id; ?>">
-		<label for="name">Name</label>
-		<input type="text" name="name" value="<?= $name; ?>" autocomplete="off" required><br>
-		<label for="description">Description</label>
-		<input type="text" name="description" value="<?= $description; ?>" autocomplete="off" required><br>
-		<label for="category">Category</label>
-		<select name="category" id="category">
-	<?php  
-		$qry_result = mysqli_query($conn, "SELECT name FROM menu_category")->fetch_all(MYSQLI_ASSOC);
-		foreach($qry_result as $category_vals):
-			$selected = ($category == $category_vals["name"]) ? "selected" : "";
-	?>
-			<option value="<?= $category_vals['name']; ?>" <?= $selected; ?> ><?= $category_vals['name']; ?></option>
-		<?php endforeach; ?>
-		</select><br>
-		<label for="price">Price</label>
-		<input type="number" min="0.00" step="0.01" name="price" value="<?= $price; ?>" autocomplete="off" required><br>
-		<label for="active">Active</label>
-		<input type="checkbox" name="active" <?= $active ? "checked" : "" ?> /> <br>
+    <?php 
+        if (isset($_POST['name'])) {
+            $reservation_id = (empty($_POST['reservation_id'])) ? null : $_POST['reservation_id'];
+            $party_size = $_POST['party_size'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $date = $_POST['reservation_date'];
+            $time = $_POST['reservation_time'];
 
-		<input type="submit" name="save" value="Save"><br>
-		<input type="button" name="cancel" value="Cancel" onClick="window.location.href='/editmenu.php';"><br>
-	</form>
-	<?php include_once('Footer.php'); ?>
+            if($reservation_id == null) {
+                $qry = "INSERT INTO reservation_table (party_size, name, email, phone, reservation_date, reservation_time) VALUES ({$party_size}, '{$name}', '{$email}', '{$phone}', '{$date}', '{$time}')";
+            }
+            else {
+                $qry = "UPDATE reservation_table SET party_size={$party_size}, name='{$name}', email='{$email}', phone='{$phone}', reservation_date= '{$date}',reservation_time= '{$time}'  WHERE reservation_id={$reservation_id}";
+            }
+
+            $qry_result = mysqli_query($conn, $qry);
+            if($qry_result) {
+                $success = True;
+            }
+
+            if(isset($error_msg)) {
+                echo "<p class='error'>".$error_msg."</p>";
+            } else if(isset($success) && $success) {
+                echo "<p class='success'>Reservation saved!</p>";
+            }
+        }
+
+        else if(!empty($_GET['reservation_id'])) {
+            $reservation_id = $_GET['reservation_id'];
+            $qry_result = mysqli_query($conn, "SELECT * FROM reservation_table where reservation_id= {$reservation_id}");
+            $restaurant_table = mysqli_fetch_assoc($qry_result);
+            $party_size = $restaurant_table["party_size"];
+            $name = $restaurant_table["name"];
+            $email = $restaurant_table["email"];
+            $phone = $restaurant_table["phone"];
+            $date = $restaurant_table["reservation_date"];
+            $time = $restaurant_table["reservation_time"];
+        } 
+        else {
+            $reservation_id = "";
+            $party_size = 0;
+            $name = "";
+            $email = "";
+            $phone = "";
+            $date = "";
+            $time = "";
+        }
+    ?>
+	<form action="reservation.php" class="form" method="POST">
+        <input type="hidden" name="reservation_id" value="<?=$reservation_id?>">
+
+        <label for="party_size">Enter a number of people:</label>
+        <input type="number" min="1" step="1" id="party_size" name="party_size" value="<?=$party_size?>" placeholder="Provide a number">
+
+        <label>Select a date:</label>
+        <input type="date" name="reservation_date" value="<?=$date?>">
+
+        <label>Select a time:</label>
+        <input type="time" name="reservation_time" value="<?=$time?>">
+
+        <label for="name">Enter your name:</label>
+        <input type="text" id="name" name="name" value="<?=$name?>" placeholder="Provide an name" required>
+
+        <label for="email">Enter your email:</label>
+        <input type="email" id="email" name="email" value="<?=$email?>" placeholder="Provide an email" required>
+
+        <label for="phone">Enter your phone number:</label>
+        <input type="tel" id="phone" name="phone" value="<?=$phone?>" placeholder="Provide an phone" required>
+
+        <input type="submit" name="Submit">
+    </form>
+	<?php
+		// Footer 
+		require_once("Footer.php"); 
+	?>
 </body>
 </html>
--- public/reservation.php
+++ public/reservation.php
@@ -7,92 +7,47 @@
 	<link rel="stylesheet" type="text/css" href="CSS/Main.css">
 </head>
 <body>
-	<?php  
-		// Header
-		require_once("Header.php");
-		require_once("utils.php");
-		checkAndStartSession();
-		$logged_in = isLoggedIn();
-
-		// Connect to MySQL
-		require_once("Connect.php");
-	?>
-    <?php 
-        if (isset($_POST['name'])) {
-            $reservation_id = (empty($_POST['reservation_id'])) ? null : $_POST['reservation_id'];
-            $party_size = $_POST['party_size'];
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $phone = $_POST['phone'];
-            $date = $_POST['reservation_date'];
-            $time = $_POST['reservation_time'];
-
-            if($reservation_id == null) {
-                $qry = "INSERT INTO reservation_table (party_size, name, email, phone, reservation_date, reservation_time) VALUES ({$party_size}, '{$name}', '{$email}', '{$phone}', '{$date}', '{$time}')";
-            }
-            else {
-                $qry = "UPDATE reservation_table SET party_size={$party_size}, name='{$name}', email='{$email}', phone='{$phone}', reservation_date= '{$date}',reservation_time= '{$time}'  WHERE reservation_id={$reservation_id}";
-            }
-
-            $qry_result = mysqli_query($conn, $qry);
-            if($qry_result) {
-                $success = True;
-            }
-
-            if(isset($error_msg)) {
-                echo "<p class='error'>".$error_msg."</p>";
-            } else if(isset($success) && $success) {
-                echo "<p class='success'>Reservation saved!</p>";
-            }
-        }
-
-        else if(!empty($_GET['reservation_id'])) {
-            $reservation_id = $_GET['reservation_id'];
-            $qry_result = mysqli_query($conn, "SELECT * FROM reservation_table where reservation_id= {$reservation_id}");
-            $restaurant_table = mysqli_fetch_assoc($qry_result);
-            $party_size = $restaurant_table["party_size"];
-            $name = $restaurant_table["name"];
-            $email = $restaurant_table["email"];
-            $phone = $restaurant_table["phone"];
-            $date = $restaurant_table["reservation_date"];
-            $time = $restaurant_table["reservation_time"];
-        } 
-        else {
-            $reservation_id = "";
-            $party_size = 0;
-            $name = "";
-            $email = "";
-            $phone = "";
-            $date = "";
-            $time = "";
-        }
-    ?>
-	<form action="reservation.php" method="POST">
-        <input type="hidden" name="reservation_id" value="<?=$reservation_id?>">
-
-        <label for="party_size">Enter a number of people:</label>
-        <input type="number" min="1" step="1" id="party_size" name="party_size" value="<?=$party_size?>" placeholder="Provide a number">
-
-        <label>Select a date:</label>
-        <input type="date" name="reservation_date" value="<?=$date?>">
-
-        <label>Select a time:</label>
-        <input type="time" name="reservation_time" value="<?=$time?>">
-
-        <label for="name">Enter your name:</label>
-        <input type="text" id="name" name="name" value="<?=$name?>" placeholder="Provide an name" required>
-
-        <label for="email">Enter your email:</label>
-        <input type="email" id="email" name="email" value="<?=$email?>" placeholder="Provide an email" required>
-
-        <label for="phone">Enter your phone number:</label>
-        <input type="tel" id="phone" name="phone" value="<?=$phone?>" placeholder="Provide an phone" required>
-
-        <input type="submit" name="Submit">
-    </form>
 	<?php
-		// Footer 
-		require_once("Footer.php"); 
-	?>
+        require_once("Header.php");
+        require_once("Connect.php");
+        require_once("utils.php");
+        checkAndStartSession();
+
+        function getByCategory($qry_result) {
+            $menu_by_cat = array();
+            foreach($qry_result as $menu_item) {
+                $cat = $menu_item['category'];
+                if(!array_key_exists($cat, $menu_by_cat)) {
+                    $menu_by_cat[$cat] = array();
+                }
+                array_push($menu_by_cat[$cat], $menu_item);
+            }
+            return $menu_by_cat;
+        }
+
+        $qry_result = mysqli_query($conn, "SELECT * FROM menu_item WHERE active>0")->fetch_all(MYSQLI_ASSOC);
+        $menu_by_cat = getByCategory($qry_result);
+
+        $category_order = array("Appetizers", "Main Dishes", "Desserts");
+
+        foreach($category_order as $cat) {
+            $menu_items = $menu_by_cat[$cat];
+            if(!array_key_exists($cat, $menu_by_cat)) {
+                continue;
+            }
+            echo "<h4 class='menuCategory'>{$cat}</h4>";
+            foreach($menu_items as $menu_item) {
+    ?>
+            <div class="menuItem">
+                <div class="menuItemName"><?= $menu_item["name"]; ?></div>
+                <div class="menuItemPrice"><?= $menu_item["price"]; ?></div>
+                <div class="menuItemDescription"><?= $menu_item["description"]; ?></div>
+            </div>
+    <?php
+            }
+        }
+
+        include_once('Footer.php'); 
+    ?>
 </body>
 </html>
